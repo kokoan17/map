5cc1c76621aaba2853fe00621c3724f0f5980d87
Konstantin <kostya1703tsoy@gmail.com>
2023-11-26 11:27:49 +0300

    check3

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,106 +1,4 @@
-const parkingTitle = document.querySelector(".active__title");
-const parkingAdress = document.querySelector(".active__adress");
-const parkingPlaces = document.querySelector(".active__places");
-
-const bottom = document.querySelector(".search__bottom");
-const resultBox = document.querySelector(".box__result");
-const inputBox = document.getElementById("input-box");
-
-const booking = document.querySelector(".booking");
-const bookingTitle = document.querySelector(".booking__title");
-const bookingAdress = document.querySelector(".booking__adress");
-
-const bookingCloseBtn = document.querySelector(".top__icon");
-bookingCloseBtn.addEventListener("click", function () {
-    booking.classList.toggle("active");
-})
-
-inputBox.onkeyup = function () {
-    let result = [];
-    let input = inputBox.value;
-    if (input.length) {
-        result = availableKeywords.filter((keyword) => {
-            return keyword.toLowerCase().includes(input.toLowerCase());
-        });
-    }
-
-    display(result);
-
-    if (!result.length) {
-        resultBox.innerHTML = "";
-    }
-}
-
-function display(result) {
-    const content = result.map((list) => {
-        return "<li onclick=selectInput(this)>" + list + "</li>";
-    });
-
-    resultBox.innerHTML = "<ul>" + content.join("") + "</ul>";
-}
-
-function selectInput(list) {
-    inputBox.value = list.innerHTML;
-    resultBox.innerHTML = "";
-}
-
-const bottomActive = document.querySelector(".bottom__active");
-const bottomBtn = document.querySelector(".bottom__btn");
-
-bottomBtn.addEventListener("click", function () {
-    booking.classList.toggle("active");
-})
-
-const searchBtn = document.querySelector(".search__btn");
-
-searchBtn.addEventListener("click", function () {
-    console.log(inputBox.value);
-    bottom.classList.toggle("active");
-    bottomActive.classList.toggle("active");
-    bottomBtn.classList.toggle("active");
-    for (let i = 0; i < parkingDots.length; i++) {
-        if (inputBox.value === parkingDots[i].name) {
-            parkingTitle.innerHTML = parkingDots[i].name;
-            parkingAdress.innerHTML = parkingDots[i].adress;
-
-            let count = 0;
-            for (key in parkingDots[i].parking) {
-                count++;
-            }
-
-            parkingPlaces.innerHTML = count;
-        }
-
-        bookingTitle.innerHTML = parkingDots[i].name;
-        bookingAdress.innerHTML = parkingDots[i].adress;
-
-        let k = 0;
-        for (key in parkingDots[i].parking) {
-            k++;
-            const bookingPlace = document.createElement("div");
-            bookingPlace.classList.add("booking__place");
-
-            const placeNum = document.createElement("div");
-            placeNum.classList.add("placeNum");
-
-
-            const placeStatus = document.createElement("div");
-            placeStatus.classList.add("placeStatus");
-
-            bookingPlace.appendChild(placeNum);
-            bookingPlace.appendChild(placeStatus);
-
-            if (k <= 6) {
-
-            }
-        }
-    }
-})
-
-ymaps.ready(init);
-
-let Moscow = [55.7522, 37.6156];
-
+// Главный объект информации
 const parkingDots = [
     {
         lat: 55.7364,
@@ -163,6 +61,18 @@ const parkingDots = [
             10: "свободно",
             11: "свободно",
             12: "занято",
+            13: "занято",
+            14: "занято",
+            15: "занято",
+            16: "занято",
+            17: "свободно",
+            18: "занято",
+            19: "занято",
+            20: "занято",
+            21: "занято",
+            22: "свободно",
+            23: "свободно",
+            24: "занято",
         },
     },
     {
@@ -188,20 +98,173 @@ const parkingDots = [
     },
 ]
 
+// Создание массива с названиями всех парковок
 let availableKeywords = [];
 
 for (let i = 0; i < parkingDots.length; i++) {
     availableKeywords.push(parkingDots[i].name)
 };
 
-console.log(availableKeywords);
+// Переменные для краткой инфы о парковке
+const parkingTitle = document.querySelector(".active__title");
+const parkingAdress = document.querySelector(".active__adress");
+const parkingPlaces = document.querySelector(".active__places");
+const bottomActive = document.querySelector(".bottom__active");
+const bottomBtn = document.querySelector(".bottom__btn");
 
-console.log(parkingDots[0].parking);
+// Наложение функции открытия окна бронирования
+bottomBtn.addEventListener("click", toOpenOrCloseBookingSection)
 
-console.log(parkingDots.length);
+// Переменные для поиска
+const bottom = document.querySelector(".search__bottom");
+const resultBox = document.querySelector(".box__result");
+const inputBox = document.getElementById("input-box");
 
+// Переменные для окна бронирования
+const booking = document.querySelector(".booking");
+const bookingTitle = document.querySelector(".booking__title");
+const bookingAdress = document.querySelector(".booking__adress");
+const searchBtn = document.querySelector(".search__btn");
+const bookingPlaces = document.querySelector(".booking__places");
+const placesLeft = document.querySelector(".places__left");
+const placesRight = document.querySelector(".places__right");
+
+// Переменные, показывающие данные о свободных местах и загруженности парковок
+const numOfAvailable = document.querySelector(".numOfAvailable");
+const wordOfAvailable = document.querySelector(".wordOfAvailable");
+const load = document.querySelector(".load");
+
+// Переменные для окна с оплатой
+const pay = document.querySelector(".pay");
+const bookingBtn = document.querySelector(".booking__btn");
+bookingBtn.addEventListener("click", function () {
+    pay.classList.toggle("active");
+})
+
+const topBtn = document.querySelector(".pay__top__icon");
+topBtn.addEventListener("click", function () {
+    pay.classList.toggle("active");
+})
+
+// Объявление функции, которая транслирует данные о парковках
+numAndWord();
+
+// Наложение функции закрытия окна бронирования при нажатии на иконку
+const bookingCloseBtn = document.querySelector(".top__icon");
+bookingCloseBtn.addEventListener("click", toOpenOrCloseBookingSection)
+
+// Функция, при которой реализуется закрытие окна бронирования
+function toOpenOrCloseBookingSection() {
+    booking.classList.toggle("active");
+}
+
+// Блок кода для реализации поиска
+inputBox.onkeyup = function () {
+    let result = [];
+    let input = inputBox.value;
+    if (input.length) {
+        result = availableKeywords.filter((keyword) => {
+            return keyword.toLowerCase().includes(input.toLowerCase());
+        });
+    }
+
+    display(result);
+
+    if (!result.length) {
+        resultBox.innerHTML = "";
+    }
+}
+
+function display(result) {
+    const content = result.map((list) => {
+        return "<li onclick=selectInput(this)>" + list + "</li>";
+    });
+
+    resultBox.innerHTML = "<ul>" + content.join("") + "</ul>";
+}
+
+function selectInput(list) {
+    inputBox.value = list.innerHTML;
+    resultBox.innerHTML = "";
+}
+// Конец блока с кодом, отвечающего за поиск
+
+// Функция присваивания класса "active"
+function toggleClassActive() {
+    bottom.classList.toggle("active");
+    bottomActive.classList.toggle("active");
+    bottomBtn.classList.toggle("active");
+}
+
+// Функционал загружающий всю инфу о паркове как и краткая инфа, так и инфа с бронированием
+searchBtn.addEventListener("click", function () {
+
+    toggleClassActive();
+
+    for (let i = 0; i < parkingDots.length; i++) {
+        if (inputBox.value === parkingDots[i].name) {
+            parkingTitle.innerHTML = parkingDots[i].name;
+            parkingAdress.innerHTML = parkingDots[i].adress;
+
+            let count = 0;
+            for (key in parkingDots[i].parking) {
+                count++;
+            }
+
+            parkingPlaces.innerHTML = count;
+
+            bookingTitle.innerHTML = parkingDots[i].name;
+            bookingAdress.innerHTML = parkingDots[i].adress;
+
+            let k = 0;
+            for (key in parkingDots[i].parking) {
+                k++;
+                const bookingPlace = document.createElement("div");
+                bookingPlace.classList.add("booking__place");
+
+                const placeNum = document.createElement("div");
+                placeNum.classList.add("placeNum");
+                placeNum.appendChild(document.createTextNode(String(k)));
+
+                const border = document.createElement("div");
+                border.classList.add("between");
+
+                const placeStatus = document.createElement("div");
+                placeStatus.classList.add("placeStatus");
+                if (parkingDots[i].parking[key] === "занято") {
+                    bookingPlace.classList.add("ocupated");
+                } else {
+                    bookingPlace.classList.add("free");
+                    bookingPlace.addEventListener("click", function () {
+                        bookingPlace.classList.toggle("active");
+                    })
+                }
+                placeStatus.appendChild(document.createTextNode(parkingDots[i].parking[key]));
+
+                bookingPlace.appendChild(placeNum);
+                bookingPlace.appendChild(placeStatus);
+
+                if (k <= (count / 2)) {
+                    placesLeft.appendChild(border);
+                    placesLeft.appendChild(bookingPlace);
+                } else {
+                    placesRight.appendChild(border);
+                    placesRight.appendChild(bookingPlace);
+                }
+            }
+        }
+    }
+})
+
+// Запуск работы карт
+ymaps.ready(init);
+
+// Координаты центра карты
+let Moscow = [55.7522, 37.6156];
+
+// Функция для работы с картой
 function init() {
-
+    // Объявление карты
     const map = new ymaps.Map("map", {
         center: Moscow,
         zoom: 12,
@@ -215,6 +278,7 @@ function init() {
     map.controls.remove("rulerControl");
     map.controls.remove("scrollZoom");
 
+    // Создание точек
     for (let i = 0; i < parkingDots.length; i++) {
         console.log(parkingDots[i].name);
         let placemark = new ymaps.Placemark([parkingDots[i].lat, parkingDots[i].lon], {
@@ -228,9 +292,7 @@ function init() {
             });
 
         placemark.events.add("click", function () {
-            bottom.classList.toggle("active");
-            bottomActive.classList.toggle("active");
-            bottomBtn.classList.toggle("active");
+            toggleClassActive();
 
             parkingTitle.innerHTML = parkingDots[i].name;
             parkingAdress.innerHTML = parkingDots[i].adress;
@@ -244,6 +306,44 @@ function init() {
 
             bookingTitle.innerHTML = parkingDots[i].name;
             bookingAdress.innerHTML = parkingDots[i].adress;
+
+            let k = 0;
+            for (key in parkingDots[i].parking) {
+                k++;
+                const bookingPlace = document.createElement("div");
+                bookingPlace.classList.add("booking__place");
+
+                const placeNum = document.createElement("div");
+                placeNum.classList.add("placeNum");
+                placeNum.appendChild(document.createTextNode(String(k)));
+
+                const border = document.createElement("div");
+                border.classList.add("between");
+
+                const placeStatus = document.createElement("div");
+                placeStatus.classList.add("placeStatus");
+                if (parkingDots[i].parking[key] === "занято") {
+                    bookingPlace.classList.add("ocupated");
+                } else {
+                    bookingPlace.classList.add("free");
+                    bookingPlace.addEventListener("click", function () {
+                        bookingPlace.classList.toggle("active");
+                        console.log("HI");
+                    })
+                }
+                placeStatus.appendChild(document.createTextNode(parkingDots[i].parking[key])); //статус
+
+                bookingPlace.appendChild(placeNum);
+                bookingPlace.appendChild(placeStatus);
+
+                if (k <= (count / 2)) {
+                    placesLeft.appendChild(border);
+                    placesLeft.appendChild(bookingPlace);
+                } else {
+                    placesRight.appendChild(border);
+                    placesRight.appendChild(bookingPlace);
+                }
+            }
         })
 
         map.geoObjects.add(placemark);
@@ -251,10 +351,6 @@ function init() {
 
     console.log(map.geoObjects);
 }
-
-const numOfAvailable = document.querySelector(".numOfAvailable");
-const wordOfAvailable = document.querySelector(".wordOfAvailable");
-const load = document.querySelector(".load");
 
 function numAndWord() {
     let len = parkingDots.length;
@@ -277,5 +373,3 @@ function numAndWord() {
         load.innerHTML = "Высокая";
     }
 }
-
-numAndWord();
